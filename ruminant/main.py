289f84264832c08ab4fae6597ec803927f3f923b
Mergen <paulthorand.2005@gmail.com>
2026-02-23 19:47:24 +0100

    added shorthand for --version

--- ruminant/main.py
+++ ruminant/main.py
@@ -201,7 +201,7 @@ def main(dev=False):
         "--shallow", action="store_true", help="Do not chew recovered blobs recursively"
     )
 
-    parser.add_argument("--version", action="store_true", help="Print version and exit")
+    parser.add_argument("--version", "-v", action="store_true", help="Print version and exit")
 
     # look for tqdm
     has_tqdm = True
